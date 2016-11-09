601de4739080b3773cb5e54d8dedb45e4c4dccf2
Leonardo <leo_rovaris@hotmail.com>
2016-11-09 14:59:31 -0200

    test case passed

--- Staircase.java
+++ Staircase.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 
 public class Staircase {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        scan.close();
+        String form = "%" + size + "s";
+        int i = 1;
+        while (i < size + 1) {
+            String print = "";
+            int j = 0;
+            while (j < i) {
+                print = String.valueOf(print) + "#";
+                ++j;
+            }
+            print = String.format(form, print);
+            System.out.println(print);
+            ++i;
+        }
 	}
 }
